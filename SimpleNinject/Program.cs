df6f9ef07b706a4f13ea0b0eec69030c29606c36
SiteDisks <peter@sitedisks.com.au>
2014-09-13 23:41:39 +1000

    Ninject Package
    Use Ninject dll to bind the interface to implementation
    binding the abstract to real

--- SimpleNinject/Program.cs
+++ SimpleNinject/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ninject;
 
 namespace SimpleNinject
 {
@@ -10,11 +11,17 @@ namespace SimpleNinject
     {
         static void Main(string[] args)
         {
+            StandardKernel kernel = new StandardKernel(); // Create IoC container
+            //kernel.Bind<IDataAccess>().To<SqlServerDal>(); //This container doing the following work:
+            kernel.Bind<IDataAccess>().To<MySqlServerDal>();
+            /*
             SqlServerDal dal = new SqlServerDal();
             MySqlServerDal mydal = new MySqlServerDal();
+            */
+            IDataAccess dal = kernel.Get<IDataAccess>();
 
-            Order order = new Order(); //All the dal implement the IDataAccess can inject here
-            order.Ida = dal;
+            Order order = new Order(dal); //All the dal implement the IDataAccess can inject here
+            //order.Ida = dal; //setter
 
             order.Add();
 
