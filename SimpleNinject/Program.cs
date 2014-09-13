90c11c57a6bda7b5a0358d31c47ca288ee2c1168
SiteDisks <peter@sitedisks.com.au>
2014-09-13 21:49:20 +1000

    Pro initial
    No DI or IoC
    Let's see what is DI / IoC benefit

---
+++ SimpleNinject/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleNinject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order();
+            order.Add();
+
+            Console.Read();
+        }
+    }
+
+    public class Order
+    {
+        private readonly SqlServerDal dal = new SqlServerDal();
+        public void Add()
+        {
+            dal.Add();
+        }
+    }
+
+    public class SqlServerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Add new record!");
+        }
+    }
+}
